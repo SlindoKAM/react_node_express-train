93931a746d9ce1b9cbd8d946423db080c9bfb777
Slindo <sngcobo@dvtsoftware.com>
2025-03-11 15:06:15 +0200

    enhance main.js with additional console logs for debugging and clarify global variable comments

--- node/main.js
+++ node/main.js
@@ -1,8 +1,15 @@
 // Below are Globals(variables) - No WINDOW
 
-// _dirname - path to the current directory
-// _filename - path to the current file (file name)
+// __dirname - path to the current directory
+// __filename - path to the current file (file name)
 // require - function to use modules (CommonJS)
 // module - info about current module (file)
 // process - info about env where the program is being executed
+
+console.log(__dirname); // prints the current directory
+console.log(__filename); // prints the current file name
+console.log(require); // prints the require function
+console.log(module); // prints the module info
+console.log(process); // prints the process info
+
 
