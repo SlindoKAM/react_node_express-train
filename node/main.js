4b3ad47623145d0c0b502af8e8fceee5c07bac7f
Slindo <sngcobo@dvtsoftware.com>
2025-03-11 15:09:07 +0200

    add periodic logging of 'hello world', to output every 2 seconds and comment out previous console logs

--- node/main.js
+++ node/main.js
@@ -6,10 +6,13 @@
 // module - info about current module (file)
 // process - info about env where the program is being executed
 
-console.log(__dirname); // prints the current directory
-console.log(__filename); // prints the current file name
-console.log(require); // prints the require function
-console.log(module); // prints the module info
-console.log(process); // prints the process info
+// console.log(__dirname); // prints the current directory
+// console.log(__filename); // prints the current file name
+// console.log(require); // prints the require function
+// console.log(module); // prints the module info
+// console.log(process); // prints the process info
 
-
+setInterval(() =>
+{
+    console.log('hello world');
+}, 2000); // prints hello world every 2 seconds
