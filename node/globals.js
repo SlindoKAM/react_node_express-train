938d170b268ededa9963a3202ac4e24f8fd73d20
Slindo <sngcobo@dvtsoftware.com>
2025-03-11 15:32:01 +0200

    add globals.js to define global variables and implement periodic logging of 'hello world'

---
+++ node/globals.js
@@ -0,0 +1,19 @@
+// Below are Globals(variables) - No WINDOW
+
+// __dirname - path to the current directory
+// __filename - path to the current file (file name)
+// require - function to use modules (CommonJS)
+// module - info about current module (file)
+// process - info about env where the program is being executed
+
+// console.log(__dirname); // prints the current directory
+// console.log(__filename); // prints the current file name
+// console.log(require); // prints the require function
+// console.log(module); // prints the module info
+// console.log(process); // prints the process info
+
+setInterval(() =>
+    {
+        console.log('hello world');
+    }, 2000); // prints hello world every 2 seconds
+    
