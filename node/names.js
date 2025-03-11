664b8c107e178e88cef51421dbe2598d8e0c5798
Slindo <sngcobo@dvtsoftware.com>
2025-03-11 15:51:32 +0200

    add console log to names.js for module testing and clarify exports

--- node/names.js
+++ node/names.js
@@ -3,3 +3,7 @@
 const Ngcobo = 'Ngcobo';
 const Mthatha = 'Mthatha';
 const secret = 'SUPER SECRET';
+
+//To test we need to console log the names using module
+console.log(module);
+//module is an object that has a property called exports
