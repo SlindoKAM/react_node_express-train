4011848e76335874cdc03225d4afaae46e0b5fea
Slindo <sngcobo@dvtsoftware.com>
2025-03-11 15:38:12 +0200

    refactor main.js to replace global variable comments with module definitions and implement a greeting function

--- node/main.js
+++ node/main.js
@@ -1,18 +1,13 @@
-// Below are Globals(variables) - No WINDOW
+//Modules
+const Ngcobo = 'Ngcobo';
+const Mthatha = 'Mthatha';
 
-// __dirname - path to the current directory
-// __filename - path to the current file (file name)
-// require - function to use modules (CommonJS)
-// module - info about current module (file)
-// process - info about env where the program is being executed
-
-// console.log(__dirname); // prints the current directory
-// console.log(__filename); // prints the current file name
-// console.log(require); // prints the require function
-// console.log(module); // prints the module info
-// console.log(process); // prints the process info
-
-setInterval(() =>
+const sayHi = (name) =>
 {
-    console.log('hello world');
-}, 2000); // prints hello world every 2 seconds
+    console.log(`Hello there ${name}`);
+}
+
+// Exporting the function
+sayHi ('Slindo');
+sayHi (Ngcobo);
+sayHi (Mthatha);
