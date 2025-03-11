9ac69c49be72262e342b33c681bb59333eb570f0
Slindo <sngcobo@dvtsoftware.com>
2025-03-11 14:19:33 +0200

    This is my first commit for the node.js project.

---
+++ node/main.js
@@ -0,0 +1,13 @@
+const amount = 12;
+
+if (amount < 10)
+{
+    console.log('small number');
+}else
+{
+    console.log('large number');
+}
+
+console.log(`hey it's my first node app!`);
+
+// run node main.js in terminal to run the code
