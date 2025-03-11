5a31625e9347c4111e303b1a93a1c674d6390423
Slindo <sngcobo@dvtsoftware.com>
2025-03-11 15:59:07 +0200

    refactor main.js and names.js to implement module imports and exports for better encapsulation

--- node/main.js
+++ node/main.js
@@ -1,11 +1,7 @@
-//Modules
-const Ngcobo = 'Ngcobo';
-const Mthatha = 'Mthatha';
+//CommomJS(library), every file in node is module (by default)
+//Modules - Encapsulated Code (only share minimum)
 
-const sayHi = (name) =>
-{
-    console.log(`Hello there ${name}`);
-}
+const names = require('./names'); // Importing the names module(This allows us to access the names module)[If you are accesing a file that is levels up you use (../names)]
 
 // Exporting the function
 sayHi ('Slindo');
--- node/names.js
+++ node/names.js
@@ -1,9 +1,12 @@
 //Names used in the project
 
+//Shared module
 const Ngcobo = 'Ngcobo';
 const Mthatha = 'Mthatha';
+//Private module(local)
 const secret = 'SUPER SECRET';
 
 //To test we need to console log the names using module
-console.log(module);
+// console.log(module);
 //module is an object that has a property called exports
+module.exports = {Ngcobo, Mthatha}; //Exporting the names
