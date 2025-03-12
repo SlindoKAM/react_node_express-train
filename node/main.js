a7e128032628f9e7155ebac9e93ee0f10fa14e18
Slindo <sngcobo@dvtsoftware.com>
2025-03-12 13:57:11 +0200

    add minds module to main.js and implement addition functionality. We can using require to import the module without assigning it to a variable. ```javascript require('./minds');

--- node/main.js
+++ node/main.js
@@ -5,6 +5,8 @@ const names = require('./names'); // Importing the names module(This allows us t
 const sayHi = require('./functions'); // Importing the functions module(This allows us to access the functions module)
 const data = require('./alternative'); // Importing the alternative module(This allows us to access the alternative module)
 
+require('./minds'); // We can require/import a file without assigning it to a variable(This will run the minds module)
+
 console.log(names);// This will print the names module object with the names Ngcobo and Mthatha.
 console.log(data);// This will print the alternative module object with items: ['item1', 'item2', 'item3'] and singlePerson: { name: 'Bhuri' }.
 
---
+++ node/minds.js
@@ -0,0 +1,9 @@
+const num1 = 11;
+const num2 = 4;
+
+function addValues()
+{
+    console.log(`The sum is: ${num1 + num2}`);
+}
+
+addValues(); //This will print the sum of the two numbers
